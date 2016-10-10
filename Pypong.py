47200b3d6161ba2fc4b737f6e835c3c1e9fd6dd8
Anthony Chavez <C0sm1x@users.noreply.github.com>
2016-10-10 13:05:07 -0500

    Added the line in the midde, changed the window caption from 'game' to 'Pypong', and drew the two players

--- Pypong.py
+++ Pypong.py
@@ -13,9 +13,18 @@ white = (255, 255, 255)
 # Creating the game window
 gameDisplay = pygame.display.set_mode((display_Width,display_Height))
 # Window title
-pygame.display.set_caption('Game')
+pygame.display.set_caption('Pypong')
 
 clock = pygame.time.Clock()
+
+def player1(x,y):
+	pygame.draw.rect(gameDisplay, white, [x, y, 3, 50], 5)
+
+def player2(x,y):
+	pygame.draw.rect(gameDisplay, white, [x, y, 3, 50], 5)
+
+def lineinthemiddle(startx, starty, endx, endy):
+	pygame.draw.line(gameDisplay, white, [startx, starty], [endx, endy], 2)
 
 running = True
 # Main game loop
@@ -25,6 +34,10 @@ while running:
 		# quiting and exiting the loop and game
 		if event.type == pygame.QUIT:
 			running = False
+
+	player1(200, 200)
+	player2(400, 200)
+	lineinthemiddle(300,0, 300, 600)
 	# Updating the display
 	pygame.display.update()
 	clock.tick(60)
