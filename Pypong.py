7b80b91c439181e0ad6985843c61ae86c8fdc6ac
Anthony <C0sm1x@users.noreply.github.com>
2017-06-09 11:27:17 -0500

    Completely re-writing the game

--- Pypong.py
+++ Pypong.py
@@ -1,192 +1,217 @@
-import pygame, random 
+import pygame
+import random
+import os
 
-pygame.init()
-# Screen dimensions
-display_Width = 640
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.DISPLAY_WIDTH = 640
+        self.DISPLAY_HEIGHT = 480
+        self.gameDisplay = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
 
-display_Height = 480
+        # Window title
+        pygame.display.set_caption("Pypong")
+        self.clock = pygame.time.Clock()
 
-# Creating the game window
-gameDisplay = pygame.display.set_mode((display_Width,display_Height))
+        #colors
 
-# Window title
-pygame.display.set_caption('Pypong')
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255, 255, 255)
 
-#defing RGB colors. Red, Blue, Green I might not use them though.
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (255, 0, 0)
+        self.FPS = 60
+        self.playerScore = 0
+        self.player2Score = 0
+        self.font = pygame.font.SysFont("arial", 80)
+        self.playerScorePosition = (self.DISPLAY_WIDTH/3, self.DISPLAY_HEIGHT/6)
+        self.player2ScorePosition = (self.DISPLAY_WIDTH/3 * 2,  self.DISPLAY_HEIGHT/6)
 
-clock = pygame.time.Clock()
+        self.running = True
 
-def player1(p1_X, p1_Y, p1_Width, p1_Height):
-	pygame.draw.rect(gameDisplay, white, [p1_X, p1_Y, p1_Width, p1_Height], )
+        self.topLineStartX = 0
+        self.topLineEndX = self.DISPLAY_WIDTH
+        self.topLineStartY = 40
+        self.topLineEndY = 40
 
-def player2(p2_X, p2_Y, p2_Width, p2_Height):
-	pygame.draw.rect(gameDisplay, white, [p2_X, p2_Y, p2_Width, p2_Height], )
-
-def lineinthemiddle(start_X, start_Y, end_X, end_Y):
-	pygame.draw.line(gameDisplay, white, [start_X, start_Y], [end_X, end_Y], 2)
-
-def ball(ball_X, ball_Y, ball_Width, ball_Height):
-    pygame.draw.circle(gameDisplay, red, (ball_X, ball_Y), ball_Width, ball_Height)
-
-# Main game loop
-def gameloop():
-    running = True
-    # Variable declaration
-    
-    x_pos = 0
-    y_pos = 0 
-
-    start_X = display_Width/2
-    start_Y = 0
-    end_X = display_Width/2
-    end_Y = 600
-
-    ball_X = start_X
-    ball_Y = 200
-    ball_Height = 0
-    ball_Width = 8
-
-    p1_X = ball_X /2
-    p1_Y = ball_Y + 3
-    p1_Width = 3
-    p1_Height = 50
-    p1_Score = 0
-
-    p2_X = ball_X * 1.5
-    p2_Y = p1_Y 
-    p2_Width = p1_Width
-    p2_Height = p1_Height 
-    p2_Score = 0
-
-    left_Boundery = 6
-    bottom_Boundery = 460
-    top_Boundery = 3
-    right_Boundery = 630 
-    top_Boundery_Collide = False
-    bottom_Boundery_Collide = False
-    # Random numers for X and Y of the ball
-    random_X_Num = random.randint(0,1)
-    random_Y_Num = random.randint(0,1) 
-    # Font
-    font = pygame.font.SysFont("arial", 50)
-    p1_Font_Render = font.render(str(p1_Score), True, white)
-    p2_Font_Render = font.render(str(p1_Score), True, white)
-    
-
-    while running:
-	    # check different events
-	    for event in pygame.event.get():
-		    # quiting and exiting the loop and game
-		    if event.type == pygame.QUIT:
-			    running = False
-		    if event.type == pygame.KEYDOWN:
-			    if event.key == pygame.K_LEFT:
-				    x_pos = -4
-			    elif event.key == pygame.K_RIGHT:
-				    x_pos = 4
-			    if event.key == pygame.K_DOWN:
-				    y_pos = 4
-			    elif event.key == pygame.K_UP:
-				    y_pos = -4	
-		    if event.type == pygame.KEYUP:
-			    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-				    x_pos = 0
-			    if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
-				    y_pos = 0
-                                
-
-	    gameDisplay.fill(black)
-	
-
-	    p1_X += x_pos	
-	    p1_Y += y_pos
-
-            p2_Y = ball_Y - 5
-
-	    player1(p1_X, p1_Y, p1_Width, p1_Height)
-	    player2(p2_X, p2_Y, p2_Width, p2_Height)
-	    lineinthemiddle(start_X, start_Y, end_X, end_Y,)
-            
-            # Player 1 bounderies
-            # Left and right bounderies
-            if p1_X > start_X - 10:
-                p1_X = start_X - 10
-            elif p1_X < left_Boundery:
-                 p1_X = left_Boundery
-            # Top and bottom bounderies
-            if p1_Y > bottom_Boundery: 
-                p1_Y = bottom_Boundery
-            elif p1_Y < top_Boundery:
-                p1_Y = top_Boundery
-
-            #Player 2 bounderies
-
-            # Left and right bounderies
-            if p2_X > right_Boundery:
-                p2_X = right_Boundery
-            elif p2_X < start_X + 10:
-                p2_X = start_X + 10
-            # Top and bottom bounderies
-            if p2_Y < top_Boundery: 
-                p2_Y = top_Boundery
-            elif p2_Y > bottom_Boundery:
-                p2_Y = bottom_Boundery
-            # Ball goes in a random direction at the start of the game
-            if random_X_Num == 0:
-                ball_X += 4
-            elif random_X_Num == 1:
-                 ball_X -= 4
-            
-            if random_Y_Num == 0:
-                ball_Y -= 4
-            elif random_Y_Num == 1:
-                ball_Y +=4
-               
-            # Bounderies for the ball
-            # Top and bottom bounderies
-            if ball_Y <  top_Boundery:
-                top_Boundery_Collide = True
-                bottom_Boundery_Collide = False
-            if top_Boundery_Collide == True:
-                random_Y_Num = 1
-            if ball_Y > bottom_Boundery:
-                bottom_Boundery_Collide = True
-                top_Boundery_Collide = False
-            if bottom_Boundery_Collide == True:
-                random_Y_Num = 0 
-            
-            # Player collisons with the ball
-            if ball_Y >  p1_Y  and ball_Y  < p1_Y + p1_Height   and ball_X == p1_X:
-                random_X_Num = 0
-            if ball_Y >  p2_Y   and ball_Y  < p2_Y + p2_Height  and ball_X == p2_X:
-                random_X_Num = 1
-
-            # If the ball goes off the right or left side of the screen
-            if ball_X > right_Boundery:
-               ball_X = start_X
-               p1_Score += 1
-               p1_Font_Render = font.render(str(p1_Score), True, white)
-               random_X_Num = random.randint(0,1)
-               random_Y_Num = random.randint(0,1)
-            if ball_X < left_Boundery:
-               ball_X = start_X 
-               p2_Score += 1
-               p2_Font_Render = font.render(str(p2_Score), True, white)
-               random_X_Num = random.randint(0,1)
-               random_Y_NUM = random.randint(0,1)
+        self.bottomLineStartX = self.topLineStartX
+        self.bottomLineEndX = self.topLineEndX
+        self.bottomLineStartY = self.DISPLAY_HEIGHT - self.topLineStartY
+        self.bottomLineEndY = self.bottomLineStartY
 
 
-            ball(ball_X, ball_Y, ball_Width, ball_Height)
+    def collision(self):
+        if player.playerX < ball.ballX + ball.BALL_WIDTH and player.playerX + player.PADDLE_WIDTH > ball.ballX and player.playerY < ball.ballY + ball.BALL_HEIGHT and player.playerY + player.PADDLE_HEIGHT > ball.ballY:
+            ball.ballXVelocity = ball.ballXVelocity * -1
+            # ball.ballYVelocity = ball.ballYVelocity * -1
+        if player2.player2X < ball.ballX + ball.BALL_WIDTH and player2.player2X + player2.PADDLE_WIDTH > ball.ballX and player2.player2Y < ball.ballY + ball.BALL_HEIGHT and player2.player2Y + player2.PADDLE_HEIGHT > ball.ballY:
+            ball.ballXVelocity = ball.ballXVelocity * -1
 
-	    # Updating the display
-            gameDisplay.blit(p1_Font_Render, (start_X/2, 10))
-            gameDisplay.blit(p2_Font_Render, (start_X * 1.5, 10))
-	    pygame.display.update()
-            clock.tick(60) 
-gameloop()
+        if ball.ballY + ball.BALL_HEIGHT > self.DISPLAY_HEIGHT - self.topLineStartY:
+            ball.ballYVelocity = ball.ballYVelocity * -1
+        if ball.ballY + ball.BALL_HEIGHT - ball.BALL_HEIGHT < self.topLineStartY:
+            ball.ballYVelocity = ball.ballYVelocity * -1
+
+    def draw(self):
+        pygame.draw.line(self.gameDisplay, self.WHITE, (self.DISPLAY_WIDTH/2 + ball.BALL_WIDTH/2, self.DISPLAY_HEIGHT - self.DISPLAY_HEIGHT + 40), (self.DISPLAY_WIDTH/2 + ball.BALL_WIDTH/2, self.DISPLAY_HEIGHT - 40))
+
+        pygame.draw.line(self.gameDisplay, self.WHITE, (self.topLineStartX, self.topLineStartY), (self.topLineEndX, self.topLineEndY))
+
+        pygame.draw.line(self.gameDisplay, self.WHITE, (self.bottomLineStartX, self.bottomLineStartY), (self.bottomLineEndX, self.bottomLineEndY))
+
+        # pygame.draw.line(self.gameDisplay, self.WHITE, (), ())
+
+
+    def bounderies(self):
+        if player.playerY > self.bottomLineStartY - player.PADDLE_HEIGHT:
+            player.playerY = self.bottomLineStartY - player.PADDLE_HEIGHT + 4
+        elif player.playerY + player.PADDLE_HEIGHT - player.PADDLE_HEIGHT < self.topLineStartY:
+            player.playerY = player.PADDLE_HEIGHT - player.PADDLE_HEIGHT + self.topLineStartY
+        if player2.player2Y > self.DISPLAY_HEIGHT - player2.PADDLE_HEIGHT:
+            player2.player2Y = self.DISPLAY_HEIGHT - player2.PADDLE_HEIGHT
+        elif player2.player2Y + player2.PADDLE_HEIGHT - player2.PADDLE_HEIGHT < self.DISPLAY_HEIGHT - self.DISPLAY_HEIGHT:
+            player2.player2Y = self.DISPLAY_HEIGHT - self.DISPLAY_HEIGHT + player2.PADDLE_HEIGHT - player2.PADDLE_HEIGHT
+
+        if ball.ballX - ball.BALL_WIDTH > self.DISPLAY_WIDTH:
+            self.playerScore = self.playerScore + 1
+            ball.ballX = self.DISPLAY_WIDTH/2
+            ball.ballY = self.DISPLAY_HEIGHT/2
+            ball.randomBallDirection = random.randint(0, 3)
+
+        if ball.ballX + ball.BALL_WIDTH < 0:
+            self.player2Score = self.player2Score + 1
+            ball.ballX = self.DISPLAY_WIDTH/2
+            ball.ballY = self.DISPLAY_HEIGHT/2
+            ball.randomBallDirection = random.randint(0, 3)
+
+    def score(self):
+        self.playerScoreDisplay = self.font.render(str(self.playerScore), True, self.WHITE)
+        self.player2ScoreDisplay = self.font.render(str(self.player2Score), True, self.WHITE)
+        self.gameDisplay.blit(self.playerScoreDisplay, (self.playerScorePosition))
+        self.gameDisplay.blit(self.player2ScoreDisplay, (self.player2ScorePosition))
+
+    def events(self):
+        for self.event in pygame.event.get():
+            if self.event.type == pygame.QUIT:
+                self.running = False
+
+            # Keyboard events and movements
+            if self.event.type == pygame.KEYDOWN:
+                if self.event.key == pygame.K_UP:
+                    player.playerYVelocity = -6
+
+                if self.event.key == pygame.K_DOWN:
+                    player.playerYVelocity = 6
+
+
+            if self.event.type == pygame.KEYUP:
+                if self.event.key == pygame.K_UP:
+                    player.playerYVelocity = 0
+
+                if self.event.key == pygame.K_DOWN:
+                    player.playerYVelocity = 0
+
+
+
+
+class Objects:
+    def __init__(self, OBJECT_WIDTH, OBJECT_HEIGHT, objectX, objectY, isBall, isPaddle, isPlayer2):
+        # Paddle
+        self.OBJECT_WIDTH = OBJECT_WIDTH
+        self.OBJECT_HEIGHT = OBJECT_HEIGHT
+        self.objectX = objectX
+        self.objectY = objectY
+        self.PADDLE_HEIGHT = self.OBJECT_HEIGHT
+        self.PADDLE_WIDTH = self.OBJECT_WIDTH
+        self.isPaddle = isPaddle
+        self.isPlayer2 = isPlayer2
+
+        # Ball
+        self.BALL_WIDTH = OBJECT_WIDTH
+        self.BALL_HEIGHT = OBJECT_HEIGHT
+        self.ballX = objectX
+        self.ballXVelocity = 4
+        self.ballY = objectY
+        self.ballYVelocity = 3
+        self.isBall = isBall
+        self.randomBallDirection = random.randint(0, 3)
+
+        self.ball = pygame.image.load(os.path.join("Sprites", "Pong_Circle.png"))
+
+        # Player
+        self.playerX = self.objectX
+        self.playerY = self.objectY
+        self.playerYVelocity = 0
+
+        # Player2
+        self.player2X = self.objectX
+        self.player2Y = self.objectY
+        self.player2YVelocity = 6
+        self.paddle = pygame.image.load(os.path.join("Sprites", "Pong_Paddle.png"))
+
+    def randomBallDir(self):
+        if self.randomBallDirection == 0:
+            self.ballX -= self.ballXVelocity
+            self.ballY -= self.ballYVelocity
+        elif self.randomBallDirection == 1:
+            self.ballX -= self.ballXVelocity
+            self.ballY += self.ballYVelocity
+        elif self.randomBallDirection == 2:
+            self.ballX += self.ballXVelocity
+            self.ballY -= self.ballYVelocity
+        elif self.randomBallDirection == 3:
+            self.ballX += self.ballXVelocity
+            self.ballY -= self.ballYVelocity
+
+    def draw(self,):
+        if self.isPaddle and self.isPlayer2 == False:
+            game.gameDisplay.blit(self.paddle, (self.playerX, self.playerY))
+
+        if self.isPaddle and self.isPlayer2:
+            game.gameDisplay.blit(self.paddle, (self.player2X, self.player2Y))
+
+        if self.isBall:
+            game.gameDisplay.blit(self.ball, (self.ballX, self.ballY))
+
+    def movement(self):
+        if self.player2Y < self.ballY:
+            self.player2Y += self.player2YVelocity
+
+        if self.player2Y > self.ballY:
+            self.player2Y -= self.player2YVelocity
+
+        if self.playerYVelocity != 0 and self.isPaddle:
+            self.playerY += self.playerYVelocity
+
+
+
+game = Game()
+line = Game()
+player = Objects(20, 63, game.DISPLAY_WIDTH/8, game.DISPLAY_HEIGHT/2, False, True, False)
+player2 = Objects(20, 63, game.DISPLAY_WIDTH - game.DISPLAY_WIDTH/8, game.DISPLAY_HEIGHT/2, False, True, True)
+ball = Objects(37, 37, game.DISPLAY_WIDTH/2, game.DISPLAY_HEIGHT/2,  True, False, False)
+
+while game.running:
+
+    game.gameDisplay.fill(game.BLACK)
+    game.bounderies()
+    game.events()
+    player.movement()
+    player2.movement()
+    player.draw()
+    player2.draw()
+    ball.draw()
+    ball.randomBallDir()
+    game.collision()
+    game.score()
+    line.draw()
+
+    print(player2.player2Y)
+    print(ball.ballY)
+
+    #print(ball.ballYVelocity)
+    #print(ball.ballXVelocity)
+    pygame.display.update()
+    game.clock.tick(game.FPS)
+
 
 pygame.quit()
 quit()
